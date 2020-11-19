023233a5396e608b5a3304c6e68ce6ce86bb0178
Aykut Satici <acsatici@gmail.com>
2020-11-19 14:16:09 -0700

    add the my_f function
    my_f is a linear function just for demonstration

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -1,5 +1,12 @@
 module MyExample
 
-# Write your package code here.
+import Random
 
-end
+greet() = print("Hello World!")
+greet_alien() = print("Hello ", Random.randstring(8))
+
+include("extra_file.jl")
+
+export my_f
+
+end # module
