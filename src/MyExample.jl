c8c97735ae98486e40ceae392e197fb2718a6dd0
Aykut Satici <acsatici@gmail.com>
2020-11-19 15:13:26 -0700

    add ForwardDiff dependency and setup safetestsets

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -1,12 +1,13 @@
 module MyExample
 
 import Random
+using ForwardDiff
 
 greet() = print("Hello World!")
 greet_alien() = print("Hello ", Random.randstring(8))
 
 include("extra_file.jl")
 
-export my_f
+export my_f, derivative_of_my_f
 
 end # module
